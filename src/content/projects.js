ac37121c1e0da8e9a932cff039b80397344995e4
priyanshu9459 <priyanshusingh9459@gmail.com>
2024-02-14 01:15:18 +0530

    ed

---
+++ src/content/projects.js
@@ -0,0 +1,42 @@
+//projects start
+const projects = [
+  {
+    name: "Infosphere Explorer",
+    description:
+      "Engineered a news app for real-time updates. Developed a robust RESTful API for seamless communication. Implemented personalized email notifications for timely updates, delivering an engaging and efficient news platform.",
+    stack: ["React", "PHP", "MySQL"],
+    sourceCode: "https://github.com/priyanshu9459/InfoSphere-Explorer.git",
+    img: "/infosp.png",
+    preview: "#",
+  },
+  {
+    name: "ThinkBinder",
+    description:
+      "This project, serves as a centralized hub for storing user notes. Designed to streamline organization and accessibility, ThinkBinder provides an intuitive platform for users to efficiently manage and retrieve their notes. Experience a seamless note-taking experience with this innovative solution.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/priyanshu9459/ThinkBinder.git",
+    img: "/thinkbinder.png",
+    preview: "#",
+  },
+  {
+    name: "Ping Pong Game",
+    description:
+      "Introducing my Ping Pong Game project, a dynamic and engaging gaming experience. Dive into the excitement of this classic game, offering thrilling rallies and challenging gameplay. Immerse yourself in the world of virtual ping pong, where precision and reflexes are key.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/priyanshu9459/Ping-Pong-Game.git",
+    img: "/ping.png",
+    preview: "#",
+  },
+  {
+    name: "Tumbola Game",
+    description:
+      "Tambola, also known as Housie or Bingo, is a popular game of chance played with numbered tickets or cards. It is often played at social gatherings, parties, and in some regions, as a form of gambling. The game is simple yet engaging, making it suitable for players of all ages.",
+    stack: ["PHP", "Postman", "MySql", "phpMyAdmin", "HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/priyanshu9459/Tumbola-Game.git",
+    img: "/tambola.png",
+    preview: "#",
+  },
+];
+
+//projects end
+export default projects;
