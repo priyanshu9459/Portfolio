0361a3adf2b250d2620aa929d391a7324f4b8bb1
priyanshu9459 <priyanshusingh9459@gmail.com>
2024-02-18 14:28:33 +0530

    yes

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,46 +1,13 @@
 //projects start
 const projects = [
   {
-    name: "Flood Detection System",
-    description:
-      "Leveraging the power of C programming, this system utilizes sensors and data processing algorithms to monitor water levels and predict potential flooding events. By analyzing real-time data and historical patterns, the system can alert authorities. ",
-    stack: ["C", "Arduino", "IOT"],
-    sourceCode: "https://github.com/priyanshu9459/Flood-Detection-System.git",
-    img: "/flood.png",
-  
-  },
-  {
-    name: "Sales Management System",
-    description:
-      "Engineered an intuitive web-based platform with HTML, CSS, and Javascript, jquery, enhancing interactivity and boosting user engagement. Specified and designed Sales Orders, optimizing the generation of Invoices and Quotations, resulting in a 20% increase in workflow efficiency.",
-      stack: ["Javascript", "PHP", "MySQL", "jQuery", "Python", "CRON"],
-    sourceCode: "https://github.com/priyanshu9459/Sales-Management.git",
-    img: "/sales.jpeg",
-  
-  },
-  {
-    name: "Tumbola Game",
-    description:
-      "Tambola, also known as Housie or Bingo, is a popular game of chance played with numbered tickets or cards. It is often played at social gatherings, parties, and in some regions, as a form of gambling. The game is simple yet engaging, making it suitable for players of all ages.",
-    stack: ["PHP", "Postman", "MySql", "phpMyAdmin", "HTML", "CSS", "JavaScript"],
-    sourceCode: "https://github.com/priyanshu9459/Tumbola-Game.git",
-    img: "/tambola.png",
-  },
-  {
     name: "Infosphere Explorer",
     description:
       "Engineered a news app for real-time updates. Developed a robust RESTful API for seamless communication. Implemented personalized email notifications for timely updates, delivering an engaging and efficient news platform.",
     stack: ["React", "PHP", "MySQL"],
     sourceCode: "https://github.com/priyanshu9459/InfoSphere-Explorer.git",
     img: "/infosp.png",
-  },
-  {
-    name: "Ping Pong Game",
-    description:
-      "Introducing my Ping Pong Game project, a dynamic and engaging gaming experience. Dive into the excitement of this classic game, offering thrilling rallies and challenging gameplay. Immerse yourself in the world of virtual ping pong, where precision and reflexes are key.",
-    stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/priyanshu9459/Ping-Pong-Game.git",
-    img: "/ping.png",
+    preview: "#",
   },
   {
     name: "ThinkBinder",
@@ -49,6 +16,25 @@ const projects = [
     stack: ["React", "NodeJS", "Express", "MongoDB"],
     sourceCode: "https://github.com/priyanshu9459/ThinkBinder.git",
     img: "/thinkbinder.png",
+    preview: "#",
+  },
+  {
+    name: "Ping Pong Game",
+    description:
+      "Introducing my Ping Pong Game project, a dynamic and engaging gaming experience. Dive into the excitement of this classic game, offering thrilling rallies and challenging gameplay. Immerse yourself in the world of virtual ping pong, where precision and reflexes are key.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/priyanshu9459/Ping-Pong-Game.git",
+    img: "/ping.png",
+    preview: "#",
+  },
+  {
+    name: "Tumbola Game",
+    description:
+      "Tambola, also known as Housie or Bingo, is a popular game of chance played with numbered tickets or cards. It is often played at social gatherings, parties, and in some regions, as a form of gambling. The game is simple yet engaging, making it suitable for players of all ages.",
+    stack: ["PHP", "Postman", "MySql", "phpMyAdmin", "HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/priyanshu9459/Tumbola-Game.git",
+    img: "/tambola.png",
+    preview: "#",
   },
 ];
 
