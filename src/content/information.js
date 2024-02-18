909b7b295a17a687747ac09ff369c3f4404dfc06
priyanshu9459 <priyanshusingh9459@gmail.com>
2024-02-18 14:20:05 +0530

    New

---
+++ src/content/information.js
@@ -0,0 +1,22 @@
+//personal information start
+
+const userData = {
+  firstName: "<Priyanshu",
+  lastName: "Kumar/>",
+  title: "About Me",
+  img: "https://shorturl.at/emNQ5",
+  description: `
+    WhoAmI? Hey there! 👋 I'm Priyanshu Kumar
+    \n As a dedicated Full Stack Developer, I bring a wealth of experience and passion for crafting innovative digital solutions. With expertise spanning front-end and back-end development, I thrive on turning ideas into reality and delivering exceptional user experiences. My commitment to staying updated with emerging technologies, coupled with my problem-solving skills, allows me to tackle complex challenges and drive impactful results. 💡🌟🚀 \n \n 🌐 **Background:** Seasoned Software Engineer in the dynamic world of information technology and services. 
+  \n  💡 **Expertise:** Versatile professional proficient in SEO strategies and a range of programming languages, including PHP, JavaScript, React, HTML5, CSS, JAVA, Python, and C. 
+  \n  🛠️ **Skills:** Creative problem solver dedicated to crafting high-quality, efficient, and user-friendly applications. 
+  \n  Let's collaborate and bring innovation to the ever-evolving tech landscape! 💻✨
+  `,
+};
+//sss
+
+//personal information end
+
+const information = { userData };
+
+export default information;
---
+++ src/content/socials.js
@@ -0,0 +1,20 @@
+const socials = [
+  {
+    icon: "github.svg",
+    url: "https://github.com/priyanshu9459",
+  },
+  {
+    icon: "linkedin.svg",
+    url: "https://www.linkedin.com/in/priyanshu-kumar-4302621b2/",
+  },
+  {
+    icon: "instagram.svg",
+    url: "https://www.instagram.com/priyanshu_._sngh/",
+  },
+  {
+    icon: "facebook.svg",
+    url: "#",
+  },
+];
+
+export default socials;
