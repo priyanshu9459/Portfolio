3e72889e6c51a34581a45e44d9aec5adba13e2ca
priyanshu9459 <priyanshusingh9459@gmail.com>
2024-02-18 14:39:22 +0530

    New Projects

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,31 +1,22 @@
 //projects start
 const projects = [
   {
-    name: "Infosphere Explorer",
+    name: "Flood Detection System",
     description:
-      "Engineered a news app for real-time updates. Developed a robust RESTful API for seamless communication. Implemented personalized email notifications for timely updates, delivering an engaging and efficient news platform.",
-    stack: ["React", "PHP", "MySQL"],
-    sourceCode: "https://github.com/priyanshu9459/InfoSphere-Explorer.git",
-    img: "/infosp.png",
-    preview: "#",
+      "Leveraging the power of C programming, this system utilizes sensors and data processing algorithms to monitor water levels and predict potential flooding events. By analyzing real-time data and historical patterns, the system can alert authorities. ",
+    stack: ["C", "Arduino", "IOT"],
+    sourceCode: "https://github.com/priyanshu9459/Flood-Detection-System.git",
+    img: "/flood.png",
+  
   },
   {
-    name: "ThinkBinder",
+    name: "Sales Management System",
     description:
-      "This project, serves as a centralized hub for storing user notes. Designed to streamline organization and accessibility, ThinkBinder provides an intuitive platform for users to efficiently manage and retrieve their notes. Experience a seamless note-taking experience with this innovative solution.",
-    stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/priyanshu9459/ThinkBinder.git",
-    img: "/thinkbinder.png",
-    preview: "#",
-  },
-  {
-    name: "Ping Pong Game",
-    description:
-      "Introducing my Ping Pong Game project, a dynamic and engaging gaming experience. Dive into the excitement of this classic game, offering thrilling rallies and challenging gameplay. Immerse yourself in the world of virtual ping pong, where precision and reflexes are key.",
-    stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/priyanshu9459/Ping-Pong-Game.git",
-    img: "/ping.png",
-    preview: "#",
+      "Engineered an intuitive web-based platform with HTML, CSS, and Javascript, jquery, enhancing interactivity and boosting user engagement. Specified and designed Sales Orders, optimizing the generation of Invoices and Quotations, resulting in a 20% increase in workflow efficiency.",
+      stack: ["Javascript", "PHP", "MySQL", "jQuery", "Python", "CRON"],
+    sourceCode: "https://github.com/priyanshu9459/Sales-Management.git",
+    img: "/sales.jpeg",
+  
   },
   {
     name: "Tumbola Game",
@@ -34,7 +25,30 @@ const projects = [
     stack: ["PHP", "Postman", "MySql", "phpMyAdmin", "HTML", "CSS", "JavaScript"],
     sourceCode: "https://github.com/priyanshu9459/Tumbola-Game.git",
     img: "/tambola.png",
-    preview: "#",
+  },
+  {
+    name: "Infosphere Explorer",
+    description:
+      "Engineered a news app for real-time updates. Developed a robust RESTful API for seamless communication. Implemented personalized email notifications for timely updates, delivering an engaging and efficient news platform.",
+    stack: ["React", "PHP", "MySQL"],
+    sourceCode: "https://github.com/priyanshu9459/InfoSphere-Explorer.git",
+    img: "/infosp.png",
+  },
+  {
+    name: "Ping Pong Game",
+    description:
+      "Introducing my Ping Pong Game project, a dynamic and engaging gaming experience. Dive into the excitement of this classic game, offering thrilling rallies and challenging gameplay. Immerse yourself in the world of virtual ping pong, where precision and reflexes are key.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/priyanshu9459/Ping-Pong-Game.git",
+    img: "/ping.png",
+  },
+  {
+    name: "ThinkBinder",
+    description:
+      "This project, serves as a centralized hub for storing user notes. Designed to streamline organization and accessibility, ThinkBinder provides an intuitive platform for users to efficiently manage and retrieve their notes. Experience a seamless note-taking experience with this innovative solution.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/priyanshu9459/ThinkBinder.git",
+    img: "/thinkbinder.png",
   },
 ];
 
